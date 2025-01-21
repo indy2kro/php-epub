f8e5848e577ce554b38eab91b75ae539a90bc155
Cristi Radu <indy2kro@gmail.com>
2025-01-21 13:49:19 +0200

    Update Exception.php

--- src/Exception.php
+++ src/Exception.php
@@ -8,29 +8,4 @@ use Exception as BaseException;
 
 class Exception extends BaseException
 {
-    /**
-     * Constructs a new PhpEpub exception.
-     *
-     * @param string $message The exception message.
-     * @param int $code The exception code.
-     * @param \Throwable|null $previous The previous throwable used for the exception chaining.
-     */
-    public function __construct(string $message = '', int $code = 0, ?\Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-
-    /**
-     * Returns a string representation of the exception.
-     */
-    public function __toString(): string
-    {
-        return sprintf(
-            "%s: [Code %d] %s\n%s",
-            self::class,
-            $this->code,
-            $this->message,
-            $this->getTraceAsString()
-        );
-    }
 }
